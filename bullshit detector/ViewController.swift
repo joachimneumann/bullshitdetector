abfa151a5e7776de49037e21b20f75831f9dd77d
Joachim Neumann <joachim@mindo.io>
2019-01-13 08:57:11 +0800

    initial commit

---
+++ bullshit detector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bullshit detector
+//
+//  Created by Joachim Neumann on 12/01/2019.
+//  Copyright © 2019 Joachim Neumann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
